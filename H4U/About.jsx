ae4977d0a24d350829421f917fa7d13b7c0a1129
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-08-24 13:54:44 +0530

    Create About.jsx

---
+++ H4U/About.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+const about =() =>{
+    return (
+        <div >
+        <Head>
+        <title>H4U</title>
+        <meta name="description" content="." />
+        </Head>
+        </div>
+    )
+}
+
+export default about
